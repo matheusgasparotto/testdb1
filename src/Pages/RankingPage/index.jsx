d8cc2ac0f7d1a7303eb360fda30ab8b2a03ce88e
Matheus <matheus.gandrade2018@gmail.com>
2021-02-16 17:52:14 -0300

    fix: error in deploy. Create a condition for localstorage empty

--- src/Pages/RankingPage/index.jsx
+++ src/Pages/RankingPage/index.jsx
@@ -7,11 +7,13 @@ const RankingPage = () => {
 
   useEffect(() => {
     const ranking = JSON.parse(window.localStorage.getItem("Ranking"));
-    ranking.sort((player1, player2) => player1.moves - player2.moves);
-    const rankingWithPositions = ranking.map((player, idx) => {
-      return { ...player, position: idx };
-    });
-    setPlayers(rankingWithPositions);
+    if (ranking) {
+      ranking.sort((player1, player2) => player1.moves - player2.moves);
+      const rankingWithPositions = ranking.map((player, idx) => {
+        return { ...player, position: idx };
+      });
+      setPlayers(rankingWithPositions);
+    }
   }, []);
   return (
     <Container>
